837c4c8293ae09076fa6046c51b317398e78aeb2
Bohdan Pylypchenko <bogdan314ylypchenko@gmail.com>
2024-03-18 22:56:29 +0200

    Add V2, Eq, Test lib, V2 Test

---
+++ libV2/V2.c
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "V2.h"
+#include <math.h>
+
+long double V2_Distance(V2 a, V2 b)
+{
+	long double lx = b.x - a.x;
+	long double ly = b.y - a.y;
+	return sqrtl(lx * lx + ly * ly);
+}
+
+V2 V2_Normalize(V2 a)
+{
+	long double m = sqrtl(a.x * a.x + a.y * a.y);
+	V2 result = { .x = a.x / m, .y = a.y / m };
+	return result;
+}
